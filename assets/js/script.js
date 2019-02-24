3af1c47be2e1b77248ffb10b21bdde3fecbfc42a
Vladislav1552 <32038448+Vladislav1552@users.noreply.github.com>
2019-02-25 00:20:35 +0300

    spb
    spb

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(function() {
+    // Owl Carousel
+    var owl = $("#big-slider");
+    owl.owlCarousel({
+      items: 1,
+      margin: 0,
+      loop: true,
+      nav: true,
+      animateOut: 'fadeOut',
+      mouseDrag: false,
+    
+      
+     
+    });
+  });
+  $(function() {
+    // Owl Carousel
+    var owl = $("#spb-js-little-slider");
+    owl.owlCarousel({
+      items: 4,
+      margin: 30,
+      loop: true,
+      nav: true,  
+      dots:false  
+     
+    });
+  });
+
