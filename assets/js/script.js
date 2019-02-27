97c86902efb68abcd97ff7372399b0e79fdd73ab
Vladislav1552 <32038448+Vladislav1552@users.noreply.github.com>
2019-02-27 23:22:20 +0300

    header_adaptiv главный бьбаннер и куча всего
    header_adaptiv главный бьбаннер и куча всего

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,8 +21,49 @@ $(function() {
       margin: 30,
       loop: true,
       nav: true,  
-      dots:false  
-     
+      dots:false,
+      responsive:{
+        0:{
+          items:1
+      
+      },
+        500:{
+          items:2
+      
+      },
+        768:{
+          items:3
+      
+      },
+        768:{
+            items:3
+        
+        },
+        1350:{
+          items:4
+      
+      },
+
+    }  
+    
     });
   });
 
+  $(document).ready(function () {
+    $('.spb-header__menu .spb-header__menu-item').hover(
+      function () {
+        $(this).addClass('hover');
+        $(".spb-header__bottom").addClass("spb-header__bottom_modal")
+        $(".spb-header__menu_lvl-2").css({"opacity":"1"})  
+     
+      },
+      function () {
+        $(this).removeClass('hover'); 
+ 
+      }
+    )
+    $(".spb-header__menu_lvl-2").mouseleave(function() {
+        $(this).css({"opacity":"0"})  
+        $(".spb-header__bottom").removeClass("spb-header__bottom_modal")
+    })
+  });
